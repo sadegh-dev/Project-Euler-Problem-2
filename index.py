7a5166aec8816aa390fb12cc8a1179241b203d3a
charlie <sadegh.rez@gmail.com>
2020-10-12 16:52:26 +0330

    v3- change num to numi

--- index.py
+++ index.py
@@ -1,22 +1,22 @@
 """
 1, 2, 3, 5, 8, 13, 21, ..., 4.000.000
 
-? = sum [ 2, 8, ..., 4.000.000 ]
+? = sumi [ 2, 8, ..., 4.000.000 ]
 
 """
 i = 1
 j = 2
 fibo = [2]
-sum = 0
+sumi = 0
 while (i+j) <= 4000000 :
-	sum = i+j
-	if sum % 2 == 0 :
-		fibo.append(sum)
+	sumi = i+j
+	if sumi % 2 == 0 :
+		fibo.append(sumi)
 	i = j
-	j = sum
+	j = sumi
 print ("Fibonacci even-valued : " + str(fibo))
 
-sum = 0
+sumi = 0
 for x in fibo:
-	sum = sum + x
-print ("sum even-valued : " + str(sum))
+	sumi = sumi + x
+print ("sum even-valued : " + str(sumi))
