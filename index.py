31d5c0f38530ec8166ff8fe0f110187fa571197b
sadegh <sadegh.rez@gmail.com>
2020-12-30 14:53:48 +0330

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,14 @@
-"""
-1, 2, 3, 5, 8, 13, 21, ..., 4.000.000
-
-? = sumi [ 2, 8, ..., 4.000.000 ]
-
-"""
 i = 1
 j = 2
 fibo = [2]
 sumi = 0
+sumj = 2
 while (i+j) <= 4000000 :
 	sumi = i+j
 	if sumi % 2 == 0 :
 		fibo.append(sumi)
+		sumj += sumi
 	i = j
 	j = sumi
 print ("Fibonacci even-valued : " + str(fibo))
-
-sumi = 0
-for x in fibo:
-	sumi = sumi + x
-print ("sum even-valued : " + str(sumi))
+print ("sum even-valued : " + str(sumj))
